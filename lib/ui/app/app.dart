2b8c9708c5ae82189ef845ea53665ba15183302a
Tomasz Wyrowiński <tomwyr@hotmail.com>
2025-12-22 14:34:54 +0100

    Reuse theme from the mobile app

--- lib/ui/app/app.dart
+++ lib/ui/app/app.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 
 import '../../i18n/strings.g.dart';
 import '../connection/connection_page.dart';
+import 'app_theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: t.app.title, home: ConnectionPage.builder());
+    return MaterialApp(
+      title: t.app.title,
+      theme: AppTheme.light,
+      builder: AppTheme.builder,
+      home: ConnectionPage.builder(),
+    );
   }
 }
